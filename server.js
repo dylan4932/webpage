327c5346291cd6e2a190ae9a78c3c6f17d024cb2
YUAN Ji <46006577+dylan4932@users.noreply.github.com>
2021-07-08 11:37:07 +0800

    add server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/')))
+
+
+app.get('/', (req, res) =>{
+    res.redirect('index.html')
+})
+
+// 404 route at the bottom for anything not found.
+app.get('*', (req, res) => {
+    res.status(404).send("404 Error<br/>Sorry! We couldn't find that page.");
+});
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+})
