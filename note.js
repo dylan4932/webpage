a12b0ea3915e8fd65d1c5a119ee8136440c034cb
YUAN Ji <dylan4932@gmail.com>
2021-11-08 16:29:09 +0800

    update popup function

--- note.js
+++ note.js
@@ -1,5 +1,15 @@
 function popup() {
     var evt = window.event || evt;
-    var popup = document.getElementById(evt.target.id.slice(0,8)+'_window');
-    popup.classList.toggle("show");
+    console.log('pop:'+evt.target.id)
+    var popup = document.getElementById(evt.target.id+'_window');
+    popup.classList.add("show");
+    // popup.className = 'note_container show'
+}
+
+function close_popup(){
+    var evt = window.event || evt;
+    console.log('close'+evt.target.id)
+    var popup = document.getElementById(evt.target.id.slice(0,9)+'_window');
+    popup.classList.remove("show");
+    // popup.className = 'note_container show'
 }
