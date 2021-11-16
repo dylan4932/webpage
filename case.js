5eb32001d90175d9f23cb9a12ac19331d44dc1b7
YUAN Ji <dylan4932@gmail.com>
2021-11-16 10:26:56 +0800

    update model2-phase2

--- case.js
+++ case.js
@@ -122,3 +122,50 @@ function report(){
     note.style.opacity = '1'
     sheet.scrollTo(0,1300)
 }
+
+function form(){
+    var evt = window.event || evt;
+
+    const type1 = document.getElementById('type1')
+    const type2 = document.getElementById('type2')
+    const type3 = document.getElementById('type3')
+    const type4 = document.getElementById('type4')
+    type1.removeAttribute('checked')
+    type2.removeAttribute('checked')
+    type3.removeAttribute('checked')
+    type4.removeAttribute('checked')
+    if(evt.target.id === 'researchp'){
+        return
+    }
+    
+    if(evt.target.id == 'form1' || evt.target.id == 'form2' || evt.target.id == 'form3'){
+        type1.removeAttribute('disabled')
+        type2.removeAttribute('disabled')
+        type3.removeAttribute('disabled')
+        type4.removeAttribute('disabled')
+        type2.setAttribute('disabled', '')
+        type3.setAttribute('disabled', '')
+        type4.setAttribute('disabled', '')
+    }
+    if(evt.target.id == 'form4'){
+        type1.removeAttribute('disabled')
+        type2.removeAttribute('disabled')
+        type3.removeAttribute('disabled')
+        type4.removeAttribute('disabled')
+    }
+    if(evt.target.id == 'form5'){
+        type1.removeAttribute('disabled')
+        type2.removeAttribute('disabled')
+        type3.removeAttribute('disabled')
+        type4.removeAttribute('disabled')
+        type1.setAttribute('disabled', '')
+    }
+    if(evt.target.id == 'form6' || evt.target.id == 'form7'){
+        type1.removeAttribute('disabled')
+        type2.removeAttribute('disabled')
+        type3.removeAttribute('disabled')
+        type4.removeAttribute('disabled')
+        type1.setAttribute('disabled', '')
+        type2.setAttribute('disabled', '')
+    }
+}
