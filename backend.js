3fe9eb202b75e1edd5b2593be2a6edeff25deb18
YUAN Ji <dylan4932@gmail.com>
2022-02-17 13:58:06 +0800

    upadte backend pages

--- backend.js
+++ backend.js
@@ -13,47 +13,47 @@ function input() {
     
     const child_content1 = document.createElement('li')
     const content1 = document.createElement('div')
-    const content1_txt = document.createTextNode('base question')
+    const content1_txt = document.createTextNode('Base Input')
     content1.append(content1_txt)
     child_content1.appendChild(content1)
 
     const child_content2 = document.createElement('li')
     const content2 = document.createElement('div')
-    const content2_txt = document.createTextNode('module2 question')
+    const content2_txt = document.createTextNode('Module 2 · Phase1 & Path gate')
     content2.append(content2_txt)
     child_content2.appendChild(content2)
 
     const child_content3 = document.createElement('li')
     const content3 = document.createElement('div')
-    const content3_txt = document.createTextNode('module2 报告管理')
+    const content3_txt = document.createTextNode('Path 管理')
     content3.append(content3_txt)
     child_content3.appendChild(content3)
 
     const child_content4 = document.createElement('li')
     const content4 = document.createElement('div')
-    const content4_txt = document.createTextNode('path 管理')
+    const content4_txt = document.createTextNode('Path Input 管理')
     content4.append(content4_txt)
     child_content4.appendChild(content4)
 
-    const child_content5 = document.createElement('li')
-    const content5 = document.createElement('div')
-    const content5_txt = document.createTextNode('pathQuestion 管理')
-    content5.append(content5_txt)
-    child_content5.appendChild(content5)
+    // const child_content5 = document.createElement('li')
+    // const content5 = document.createElement('div')
+    // const content5_txt = document.createTextNode('pathQuestion 管理')
+    // content5.append(content5_txt)
+    // child_content5.appendChild(content5)
 
-    const child_content6 = document.createElement('li')
-    const content6 = document.createElement('div')
-    const content6_txt = document.createTextNode('moduleOutput 管理')
-    content6.append(content6_txt)
-    child_content6.appendChild(content6)
+    // const child_content6 = document.createElement('li')
+    // const content6 = document.createElement('div')
+    // const content6_txt = document.createTextNode('moduleOutput 管理')
+    // content6.append(content6_txt)
+    // child_content6.appendChild(content6)
 
 
     content.appendChild(child_content1)
     content.appendChild(child_content2)
     content.appendChild(child_content3)
     content.appendChild(child_content4)
-    content.appendChild(child_content5)
-    content.appendChild(child_content6) 
+    // content.appendChild(child_content5)
+    // content.appendChild(child_content6) 
     container.appendChild(content)
 
 }
@@ -112,7 +112,8 @@ function clear_path(){
 }
 
 function openForm() {
-    document.getElementById("myForm1").style.display = "block";
+    document.getElementById("myForm1").style.display = "block"
+    document.getElementById("myForm1").style.filter = "brightness(100%)";
 }
   
 function closeForm() {
